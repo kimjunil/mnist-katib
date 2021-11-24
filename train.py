199048f72b7cea6a3f98fab66ded0d5a054a3f33
Kim Junil <iam@junil.kim>
2021-11-24 14:18:40 +0900

    initial commit

---
+++ train.py
@@ -0,0 +1,45 @@
+from typing import get_args
+import tensorflow as tf
+import argparse
+from utils import send_message_to_slack
+from utils import request_deploy_api
+import numpy as np
+import dvc.api
+
+
+def train():
+    print("TensorFlow version: ", tf.__version__)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning_rate', default=0.01, type=float)
+    parser.add_argument('--dropout', default=0.2, type=float)
+    parser.add_argument('--epochs', default=5, type=int)
+    args = parser.parse_args()
+    
+    with dvc.api.open(
+        'data/dataset.npz',
+        repo='https://github.com/ssuwani/dvc-tutorial',
+        mode="rb"
+    ) as fd:
+        dataset = np.load(fd)
+        train_x = dataset["train_x"]
+        train_y = dataset["train_y"]
+        test_x = dataset["test_x"]
+        test_y = dataset["test_y"]
+
+    train_x, test_x = train_x / 255.0, test_x / 255.0
+    
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dropout(args.dropout),
+      tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    print("Training...")
+    training_history = model.fit(train_x, train_y, epochs=5, validation_data=(test_x, test_y))
+    print("Average test loss: ", np.average(training_history.history['loss']))
+
+if __name__ == '__main__':
+    train()
